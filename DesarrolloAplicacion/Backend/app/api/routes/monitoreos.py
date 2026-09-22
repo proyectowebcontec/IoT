3b85f15c2998b83a2f4164613c6622ffab1becfd
Katherinne Gomez <gdxkg@local.contecindustrial.com>
2026-09-22 11:41:55 -0600

    feat(backend): endpoint para historial de monitoreos

--- DesarrolloAplicacion/Backend/app/api/routes/monitoreos.py
+++ DesarrolloAplicacion/Backend/app/api/routes/monitoreos.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
+from utils.validaciones import _validar_rango_fechas
 from models.entradasensor import EntradaSensor
 from models.errores import RecursoNoEncontradoError
 from schemas.medicionpunto import MedicionPunto
+from schemas.monitoreo import Monitoreo
 from database.mongodb import monitoreos_collection
-
+from datetime import datetime
 
 router = APIRouter(
     prefix="/api/monitoreos",
@@ -37,6 +39,36 @@ def obtener_count_monitoreos():
     return {
         "total_monitoreos": total
     }
+
+@router.get("/{id_dispositivo}/{f_inicio}/{f_fin}", response_model=list[Monitoreo])
+def obtener_monitoreos(
+    id_dispositivo: str,
+    f_inicio: datetime,
+    f_fin: datetime
+):
+
+    _validar_rango_fechas(f_inicio, f_fin)
+    
+    monitoreos = list(
+        monitoreos_collection.find({
+            "IDDispositivo": id_dispositivo,
+            "FechaMonitoreo": {
+                "$gte": f_inicio,
+                "$lte": f_fin
+            }
+        })
+    )
+
+    for monitoreo in monitoreos:
+        monitoreo["_id"] = str(monitoreo["_id"])
+
+    
+    if not monitoreos:
+        raise RecursoNoEncontradoError(
+            f"No hay mediciones para el dispositivo '{id_dispositivo}'"
+        )
+
+    return monitoreos
 
 # A diferencia del enpoint en dashboard extrae más de 50 registros para poder mostrarlos en la tabla
 @router.get("/historico/{id_dispositivo}/{entrada}", response_model=list[MedicionPunto])
